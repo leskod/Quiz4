8f9c901afa82623f5ad9a82de0bf767e948f5ac4
Dereck Lesko <lesko@moravian.edu>
2014-02-05 15:48:13 -0500

    Initial Commit

---
+++ Quiz4/Quiz4AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Quiz4AppDelegate.h
+//  Quiz4
+//
+//  Created by Lesko, Dereck on 2/5/14.
+//  Copyright (c) 2014 Lesko, Dereck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Quiz4AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
